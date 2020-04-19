71a59b589b9d4f0fd0bd02c33e93fd0d48d50fde
Robbert Müller <spam.me@grols.ch>
2020-04-19 15:04:47 +0200

    Bed coordination fixes after moving x-end-stop

--- Configuration.h
+++ Configuration.h
@@ -1122,14 +1122,14 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 225
+#define X_BED_SIZE 230
 #define Y_BED_SIZE 225
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
+#define X_MIN_POS 0 - 2
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
+#define X_MAX_POS X_BED_SIZE +5
 #define Y_MAX_POS Y_BED_SIZE
 #define Z_MAX_POS 300
 
